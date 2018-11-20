7555f021526002cd7d8a5a665cf878e69ecb879a
FredJiang <jiang.fei@ffcg.se>
2018-11-20 17:48:04 +0800

    t

---
+++ LearnCosmosDB/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using CrossPartitionQueries;
+using Microsoft.Azure.Documents.Client;
+
+namespace LearnCosmosDB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CrossPartitionQueriesDemo.Run().Wait();
+        }
+    }
+}
